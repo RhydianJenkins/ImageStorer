268e2c897ab8ba9790b349910e5c7e45f67c3499
Rhydian Jenkins <rhydian.jenkins@wcbs.co.uk>
2021-12-19 00:20:14 +0000

    Composer used for autoloading

--- index.php
+++ index.php
@@ -1,15 +1,10 @@
 <?php
 
+require __DIR__ . '/vendor/autoload.php';
+
 use src\Main;
 use src\Saver;
 use src\Validator;
-
-spl_autoload_register(function ($class) {
-    $file = './src/' . $class . '.php';
-    if (file_exists($file)) {
-        require $file;
-    }
-});
 
 $main = new Main(new Saver(), new Validator());
 $main->saveImage('/tmp/image.jpg');
